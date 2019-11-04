594772e0a865101d87bf73e38c5ad4df5363ab18
llorencroma <52706876+llorencroma@users.noreply.github.com>
2019-11-04 12:25:57 +0100

    Update install.sh

--- aby/install.sh
+++ aby/install.sh
@@ -1,17 +1,7 @@
 
 # get ABY with the examples of our semester project
-git clone -b eurecom --recursive https://github.com/llorencroma/ABY.git
+git clone --recursive https://github.com/llorencroma/ABY.git -b eurecom
 cd ABY
-git checkout -b eurecom 
-
-# copy our working examples 
-for EX in mult3 crosstabs innerprod
-do
-  cp -r ~/source/$EX src/examples/
-  echo 'add_subdirectory('$EX')' >> src/examples/CMakeLists.txt
-done
-
-cp ~/source/geninput.py src/examples/
 
 # build ABY
 mkdir build && cd build
